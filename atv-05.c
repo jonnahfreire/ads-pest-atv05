1504626e49dbdd458d6a729dac1b98e9d16e3ce9
jonnahfreire <jonnas.freire17@gmail.com>
2021-07-07 15:47:35 -0300

    ..

--- atv-05.c
+++ atv-05.c
@@ -12,9 +12,9 @@
 */
 
 // Function Prototypes
-void *showTeam(int i, char* times[8][2]);
+void *showTeam(int i, char* times[8][2], int etapa);
 void *showTeamNames(char *times[8][2]);
-void *iniciarCampeonato(char *times[8][2]);
+void *iniciarCampeonato(char *times[8][2], int etapa);
 void clear();
 
 
@@ -29,15 +29,16 @@ int main(){
         {"Irlanda", "Chile"},
         {"Peru", "Colombia"}
     };
-
-    iniciarCampeonato(times);
+    int etapa = 1;
+    iniciarCampeonato(times, etapa);
 
     return 0;
 }
 
 // Functions
-void *showTeam(int i, char* times[8][2]){
-    printf("\n\tJOGO %d\n", i+1);
+void *showTeam(int i, char* times[8][2], int etapa){
+    printf("\n  JOGO %d - ETAPA %d\n", i+1, etapa);
+    times[i][0] != '\0' &&
     printf("\n%s - %s\n", times[i][0], times[i][1]);
     printf("_______________________\n");
 }
@@ -48,6 +49,7 @@ void *showTeamNames(char *times[8][2]){
     printf(" TIMES DO CAMPEONATO \n");
     printf("---------------------\n");
     for(i = 0; i < 8; i++){
+        times[i][0] != '\0' &&
         printf("%s - %s\n", times[i][0], times[i][1]);
     }
     printf("_____________________\n");
@@ -63,62 +65,108 @@ void clear(){
     #endif
 }
 
-void *iniciarCampeonato(char *times[8][2]) {
-    int placar[8][2];
+int getTimesLength(char *times[8][2]) {
+    int i, length;
+    for(i = 0; i < 8; i++){
+        if((times[i][0] && times[i][1] != '\0'))
+           length++;
+    }
+    return length;
+}
+
+void *iniciarCampeonato(char *times[8][2], int etapa) {
+    unsigned int placar[8][2];
+    /* = {
+        {5, 4},
+        {3, 4},
+        {2, 2},
+        {3, 3},
+        {1, 2},
+        {3, 6},
+        {1, 2},
+        {2, 3},
+    };*/
     int i, e, winnerAmount, draws;
     char *winners[8];
 
-
     for(i = 0; i < 8; i++){
-        clear();
-        showTeamNames(times);
-        showTeam(i, times);
+        if((times[i][0] != '\0') && (times[i][1] != '\0')){
+            clear();
+            showTeamNames(times);
+            showTeam(i, times, etapa);
 
-        for(e = 0; e < 2; e++){
-            printf("Digite o placar do time %d: ", e+1);
-            scanf("%d", &placar[i][e]);
-        }
+            for(e = 0; e < 2; e++){
+                ((times[i][0] != '\0') && (times[i][1] != '\0')) &&
+                printf("Digite o placar do time %d: ", e+1);
+                scanf("%d", &placar[i][e]);
+            }
 
-        if(placar[i][0] > placar[i][1]){
-            //printf("VENCEDOR: %s\n", times[i][0]);
-            winners[i] = times[i][0];
-            winnerAmount += 1;
+            if(placar[i][0] > placar[i][1]){
+                winners[i] = times[i][0];
+                winnerAmount += 1;
 
-        }else if(placar[i][0] == placar[i][1]){
-            do {
-                printf("\nEMPATE: %s - %s\n", times[i][0], times[i][1]);
-                draws += 1;
-                printf("======= DESEMPATE =======\n");
-                for(e = 0; e < 2; e++){
-                    printf("Digite os penaltis do time %d: ", e+1);
-                    scanf("%d", &placar[i][e]);
-                }
-                if(placar[i][0] > placar[i][1]){
-                    //printf("\nVENCEDOR: %s\n", times[i][0]);
-                    winners[i] = times[i][0];
-                    winnerAmount += 1;
-                }else if(placar[i][0] < placar[i][1]){
-                    winners[i] = times[i][1];
-                    winnerAmount += 1;
-                    //printf("\nVENCEDOR: %s\n", times[i][1]);
-                }else {
+            }else if(placar[i][0] == placar[i][1]){
+                do {
+                    if(times[i][0] == '\0') break;
+                    printf("\nEMPATE: %s - %s\n", times[i][0], times[i][1]);
                     draws += 1;
-                }
-            }while(placar[i][0] == placar[i][1]);
+                    printf("======= DESEMPATE =======\n");
+                    for(e = 0; e < 2; e++){
+                        printf("Digite os penaltis do time %d: ", e+1);
+                        scanf("%d", &placar[i][e]);
+                    }
+                    if(placar[i][0] > placar[i][1]){
+                        winners[i] = times[i][0];
+                        winnerAmount += 1;
+                    }else if(placar[i][0] < placar[i][1]){
+                        winners[i] = times[i][1];
+                        winnerAmount += 1;
+                    }else {
+                        draws += 1;
+                    }
+                }while(placar[i][0] == placar[i][1]);
 
-        }else {
-            //printf("VENCEDOR: %s\n", times[i][1]);
-            winners[i] = times[i][1];
-            winnerAmount += 1;
+            }else {
+                winners[i] = times[i][1];
+                winnerAmount += 1;
+            }
         }
+
     }
 
 
-    printf("Quantidade de vencedores: %d\n", winnerAmount);
-    printf("Quantidade de Empates: %d\n", draws);
+    //printf("Quantidade de vencedores: %d\n", winnerAmount);
+    //printf("Quantidade de Empates: %d\n", draws);
 
-    printf("\n====== WINNERS ======\n");
+    printf("\n====== VENCEDORES ETAPA %d ======\n", etapa);
     for(i = 0; i < 8; i++){
         printf("%s\n", winners[i]);
     }
+
+    etapa += 1;
+
+    for(i = 0; i < 8; i++){
+        for(e = 0; e < 2; e++){
+            times[i][e] = '\0';
+        }
+    }
+
+
+    int a=0;
+    for(i = 0; i < 8; i+=2){
+        for(e = 0; e < 2; e++){
+            times[i][e] = (e == 1) ? winners[i+1] : winners[i];
+            a++;
+        }
+    }
+    printf("\n\nNOVOS TIMES A JOGAR\n");
+
+    for(i = 0; i < 8; i++){
+        times[i][0] != '\0' && printf("%s - %s\n", times[i][0], times[i][1]);
+    }
+
+    for(i = 0; i < 8; i++) winners[i] = '\0';
+
+    iniciarCampeonato(times, etapa);
+
 }
